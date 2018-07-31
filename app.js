111d36e2cedc7ce0b5760a8f0a03c4b71445889b
Artak <artak.avanesyan93@gmail.com>
2018-07-31 16:18:32 +0300

    delete some classes

--- app.js
+++ app.js
@@ -1,25 +1,6 @@
-// var result = document.getElementById('result-block');
-// var number1 = document.querySelectorAll('.number-1');
-// var number2 = document.querySelectorAll('.number-2');
-// var plus = document.querySelectorAll('.plus');
-// var minus = document.querySelectorAll('.minus');
-// var multiplication = document.querySelectorAll('.multiply');
-// var division = document.querySelectorAll('.divide');
-
-// plus.addEventListener('click', function() {
-// 	var result = document.getElementById('result-block');
-// var number1 = document.querySelectorAll('.number-1');
-// var number2 = document.querySelectorAll('.number-2');
-// 	result.textContent = number1.value + number2.value; 
-// })
-
-// var num1 = document.querySelector(".number-1");
-// var num2 = document.querySelector(".number-2");
-
-
 var resultOfOperation = document.getElementById("result-block");
 var prevResult = +resultOfOperation.textContent;
-	
+
 var plus = document.querySelector(".plus");
 plus.addEventListener('click', function () {
 	var number1 = +document.getElementById("input1").value;
@@ -51,13 +32,3 @@ division.addEventListener('click', function () {
 	var resultOfDivision = number1 / number2;
 	resultOfOperation.textContent = prevResult + resultOfDivision;
 })
-
-
-/*function add() {
-	var number1 = +document.getElementById("input1").value;
-	var number2 = +document.getElementById("input2").value;
-	var resultOfOperation = document.getElementById("result-block");
-	var prevResult = +resultOfOperation.textContent;
-	var resultOfAdd = number1 + number2;
-	resultOfOperation.textContent = prevResult + resultOfAdd;
-}*/
